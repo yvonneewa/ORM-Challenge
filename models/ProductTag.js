491c49b62fb5aca75c7cc24b2665ecee5f9b93a5
Yvonne <yowantings@gmail.com>
2024-05-25 16:18:28 -0600

    moved files so i can push

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
 
--- models/Tag.js
+++ models/Tag.js
@@ -2,21 +2,21 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
-  
-    // define columns
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      tag_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+
+  // define columns
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
